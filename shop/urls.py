cdad3a9cbd9ecea0804c7d1b40508f8a2209de98
unknown <abnvarghese@gmail.com>
2024-04-25 16:50:48 +0530

    second commit

--- shop/urls.py
+++ shop/urls.py
@@ -10,8 +10,3 @@ urlpatterns = [
     path('search/',include('search_app.urls')),
     path('cart/',include('cart.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                         document_root=settings.MEDIA_ROOT)
--- shopapp/views.py
+++ shopapp/views.py
@@ -11,7 +11,7 @@ def allProdCat(request,c_slug=None):
         products_list = Product.objects.all().filter(category=c_page,available=True)
     else:
         products_list = Product.objects.all().filter(available=True)
-    paginator=Paginator(products_list,6)
+    paginator=Paginator(products_list,8)
     try:
         page=int(request.GET.get('page','1'))
     except:
